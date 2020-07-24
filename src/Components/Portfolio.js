1c7287d510c2fcec42a13c9036537b4947d94a2e
Kristin Aune <kristinaune@Kristins-MacBook-Pro.local>
2020-07-24 20:50:57 +0200

    new update

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Sjekk ut min første React-applikasjon; en Github-søkemotor</h1>
+            <h1>Dette er mitt første prosjekt, en statisk nettside</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
